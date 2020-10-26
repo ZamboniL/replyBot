904c999d2ea150d25b6ff8f75f485677c3d65fd0
Lucas Zamboni Orioli <71368763+ZamboniL@users.noreply.github.com>
2020-10-26 18:32:49 -0300

    New config settings, now uses a config.txt file

--- replyBot/replyBot.py
+++ replyBot/replyBot.py
@@ -5,37 +5,66 @@ from time import sleep
 # If the user posted a new tweet this BOT then replys to his tweet
 
 # Defines the target of this BOT
-USER = 'jairbolsonaro'
-
+BOT_CONFIG = {}
 
 def main():
-    print("authorizing")
+    config_bot()
+
+    print('Authorizing')
     api = auth()
+
+    try:
+        api.verify_credentials()
+        print('Authentication OK')
+    except:
+        print('Error during authentication')
 
     while True:
         # The BOT checks for new tweets every 10 minutes
-        sleep(600)
+        sleep(BOT_CONFIG['TIME_TO_CHECK'])
         print("starting new check")
         postId = lastPostId()
 
         # If there's a new tweet, reply to it
         if postedNewTweet(api, postId):
             newPostId = lastPostId()
-            api.update_status(status = 'Vai se fuder @' + USER, in_reply_to_status_id = newPostId)
+            api.update_status(status = BOT_CONFIG['MESSAGE'] + ' @' + BOT_CONFIG['TARGET_USER'], in_reply_to_status_id = newPostId)
             print("posted reply")
         
+
+# config.txt information pickup
+def config_bot():
+    with open('config.txt', 'r') as config:
+        for line in config:
+            (key, value) = line.split(':')
+
+            if 'TIME_TO_CHECK' in key:
+                if value != '':
+                    BOT_CONFIG[key] = int(value)
+                else:
+                    BOT_CONFIG[key] = 600
+            elif key in ['TARGET_USER', 'ACCESS_TOKEN', 'ACCESS_TOKEN_SECRET', 'CONSUMER_KEY', 
+                        'CONSUMER_SECRET', 'TARGET_USER', 'MESSAGE', 'LAST_ID_FILE']:
+                if value == '':
+                    print('Missing ' + key + " Parameter")
+                    exit(1)
+                else:
+                    BOT_CONFIG[key] = value.strip('\n')
+            elif key in 'TARGET_TEXT':
+                if value != '':
+                    BOT_CONFIG[key] = value.strip('\n')
+                else:
+                    BOT_CONFIG[key] = 0
+            else:
+                print('Invalid ' + key + ' Parameter')
+
+
 
 # Checks the authorization for the API
 def auth():
     # Opens a file with the API credentials
-    with open('twitter-keys.txt', 'r') as keys:
-        access_token = keys.readline().strip('\n')
-        access_token_secret = keys.readline().strip('\n')
-        consumer_key = keys.readline().strip('\n')
-        consumer_secret = keys.readline().strip('\n')
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth = tweepy.OAuthHandler(BOT_CONFIG['CONSUMER_KEY'], BOT_CONFIG['CONSUMER_SECRET'])
+    auth.set_access_token(BOT_CONFIG['ACCESS_TOKEN'], BOT_CONFIG['ACCESS_TOKEN_SECRET'])
     api = tweepy.API(auth)
     # Returns the authorized command
     return api
@@ -43,7 +72,7 @@ def auth():
 
 # Serves to check the last tweet ID that is saved on the last_tweet file
 def lastPostId():
-    with open('last_tweet.txt', 'r') as last_tweet:
+    with open(BOT_CONFIG['LAST_ID_FILE'], 'r') as last_tweet:
         lastId = last_tweet.readline().strip('\n')
         return lastId
 
@@ -52,18 +81,27 @@ def lastPostId():
 def postedNewTweet(api, since_id):
 
     # See if there's a new tweet posted after the since_id
-    newTweetCheck = tweepy.Cursor(api.user_timeline, id=USER, since_id = since_id).items(1)
+    newTweetCheck = tweepy.Cursor(api.user_timeline, id=BOT_CONFIG['TARGET_USER'], since_id = since_id).items(1)
     isNewTweet = 0
 
     for tweet in newTweetCheck:
         print(tweet.text)
+        isTextOnTweet = tweet.text
         isNewTweet = tweet.id
 
-    # If there was a new tweet, overwrite the old one id with this new
+    targetText = BOT_CONFIG['TARGET_TEXT'].lower()
+    
     if isNewTweet > 1:
-        with open("last_tweet.txt", 'w') as last_tweet:
-            last_tweet.write(str(isNewTweet))
-        return True
+        if targetText != 0:
+            if isTextOnTweet in targetText:
+                # If there was a new tweet, overwrite the old one id with this new
+                with open(BOT_CONFIG['LAST_ID_FILE'], 'w') as last_tweet:
+                    last_tweet.write(str(isNewTweet))
+                    return True
+        else:
+            with open(BOT_CONFIG['LAST_ID_FILE'], 'w') as last_tweet:
+                    last_tweet.write(str(isNewTweet))
+                    return True
     
     return False
 
